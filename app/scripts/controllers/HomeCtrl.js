0dbb274cada36e942b20cb70908bc44467d10453
sean21502 <seanhite@gmail.com>
2017-06-19 20:48:17 -0400

    checkpoint 4

--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,13 +1,20 @@
 (function() {
-	function HomeCtrl(Room) {
+	function HomeCtrl(Room, Message, $scope) {
 		this.mainTitle = "Bloc Chat";
 		this.rooms = Room.all;
+		/*this.send = send;*/
+		
+		var changeRooms = function(room) {
+			$scope.currentRoom = room;
+			$scope.currentMessages = Message.getByRoomId($scope.currentRoom.$id);
+		}
+		this.changeRooms = changeRooms;
 	
     }
 
 	
     angular
         .module('blocChat')
-        .controller('HomeCtrl', ['Room', HomeCtrl]);
+        .controller('HomeCtrl', ['Room','Message','$scope', HomeCtrl]);
 })();
 
--- app/scripts/services/message.js
+++ app/scripts/services/message.js
@@ -1,7 +1,14 @@
 (function() {
 	function Message($firebaseArray) {
     	var Message = {};
-    	var ref = firebase.database().ref().child("messages");
+    	var ref = firebase.database().ref().child("Messages");
+		var messages = $firebaseArray(ref);
+		
+		Message.getByRoomId = function(roomId) {
+       		var filteredRef = ref.orderByChild("roomId").equalTo(roomId);
+			var roomMessages = $firebaseArray(filteredRef);
+			return roomMessages;
+    	};
 
     	return Message;
   	}
